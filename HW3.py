a980ae64aba3acf3f65e4952e0b16102da24dc3d
Nicholas Rodriguez <niickrodriguez@ou.edu>
2023-02-21 21:59:46 -0600

    added main

--- HW3.py
+++ HW3.py
@@ -1,10 +1,31 @@
-def sum (array):
-    global sum
-    sum = 0
-    for i in array:
-        sum+= i
 
+def main():
+    print("Enter five integers")
+    arr = []
+    for i in range(5):
+        num = int(input("Enter an integer: "))
+        arr.append(num)
+
+    print("The contents of the array are:")
+    for numb in range(5):
+        print(numb + 1)
+
+    print("the sum is")
+    print(sum(arr))
+
+    print("the product of all nums is")
+    print(product(arr))
+
+
+def sum (array):
+    total = 0
+    for i in array:
+        total+= i
+    return total
 def product (array2):
-    sum = 0
+    total = 1
     for i in array2:
-        sum+=i
+        total *= i
+    return total
+
+main()
