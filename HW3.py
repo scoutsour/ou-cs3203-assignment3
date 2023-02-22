a5cc42f0a9f295c901f9d567f3768ae676c647c4
Nicholas Rodriguez <niickrodriguez@ou.edu>
2023-02-21 22:21:36 -0600

    adding my reverse to my main and then pushing to new branch

--- HW3.py
+++ HW3.py
@@ -1,4 +1,37 @@
+
+def main():
+    print("Enter five integers")
+    arr = []
+    for i in range(5):
+        num = int(input("Enter an integer: "))
+        arr.append(num)
+
+    print("The contents of the array are:")
+    for numb in range(5):
+        print(numb + 1)
+
+    print("the sum is")
+    print(sum(arr))
+
+    print("the product of all nums is")
+    print(product(arr))
+
+    print("reverse order")
+    print(reverse(arr)) # adding commit message for branch1
+
 def sum (array):
-    sum = 0
+    total = 0
     for i in array:
-        sum+= i
+        total+= i
+    return total
+def product (array2):
+    total = 1
+    for i in array2:
+        total *= i
+    return total
+
+
+def reverse(arr):
+  return arr[::-1]
+
+main()
